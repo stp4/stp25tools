bdd2ed8e2bf016e6b287416998896ca2cbf8003d
stp4 <w.peter@statistik-peter.at>
2021-11-26 09:42:19 +0100

    Neue Funktionen

---
+++ R/auc_trapezoid.R
@@ -0,0 +1,73 @@
+#' Area Under the Curve
+#'
+#' Method trapezoid
+#'
+#' @param ...  value 
+#' @param time time 
+#' @param na.rm  na.rm
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#'  dat <-
+#' data.frame(
+#'   t0 = c(1, 1, 1),
+#'   t2 = c(1, 2, 3),
+#'   t3 = c(1, 3, 5),
+#'   t4 = c(1, 3, 6)
+#' )
+#' 
+#' #apply(dat, 1, function(x)
+#'  # sum(diff(time) * zoo::rollmean(x, 2)))
+#' time = c(0, 1, 2, 3)
+#' auc_trapezoid(1,1,1,1)
+#' auc_trapezoid(dat )
+#' transform(dat, auc= auc_trapezoid( t0, t2, t3, t4, time= time))
+#' 
+auc_trapezoid <- function(...,
+                          time = NULL,
+                          na.rm = FALSE) {
+  x <- list(...)
+  
+  if (is.data.frame(x[[1]])) {
+    x <-  as.matrix(x[[1]])
+  }
+  else{
+    x <- matrix(unlist(x), ncol = length(x))
+  }
+  
+  if (is.null(time)) {
+    time <- seq_len(ncol(x))
+    warning("time is missing I use : ", paste(time, collapse = ", "), "\n")
+  }
+  
+  apply(x, 1, function(x1)
+    calc_auc_trapezoid(x1, time, na.rm))
+}
+
+
+#' stolen from  DescTools::AUC
+calc_auc_trapezoid <-
+  function (x, time = seq_along(x) ,
+            na.rm = FALSE)
+  {
+    if (na.rm) {
+      idtime <- na.omit(cbind(time, x))
+      time <- time[idtime]
+      x <- x[idtime]
+    }
+    if (length(time) != length(x))
+      stop("length time must equal length x")
+    idtime <- order(time)
+    time <- time[idtime]
+    x <- x[idtime]
+    sum((apply(cbind(x[-length(x)], x[-1]), 1, mean)) *  (time[-1] - time[-length(time)]))
+  }
+
+
+
+
+
+
---
+++ R/na-approx.R
@@ -0,0 +1,69 @@
+#' Replace NA by Interpolation
+#'
+#' @param x an object.
+#' @param ... 	further arguments passed to methods.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' dat <- data.frame(
+#'   u = c(NA, 2,  3,  4,  6,  NA, 7, NA),
+#'   v = c(NA, NA, NA, NA, 6,  4,  7, 2),
+#'   w = c(NA, 2,  NA, 4,  6,  4,  7, 2),
+#'   x = c(NA, 2,  3,  7,  6,  4,  7, NA),
+#'   y = c(NA, 7,  2,  NA, 1,  9,  3, 4),
+#'   z = c( 1, 7,  2,  8,  NA, 9,  3, 4)
+#' )
+#' 
+#' na_approx(dat$z)
+#' na_approx(dat)
+#' na_approx(as.matrix(dat))
+#' 
+na_approx <- function(x, ...) {
+  UseMethod("na_approx")
+}
+
+
+#'
+#' @param fun Interpolation  zoo::na.approx(z)  zoo::na.fill(z, "extend")  or 
+#' @param min.length min length
+#'
+#' @rdname na_approx
+#' @export
+na_approx.default <-
+  function(x,
+           fun = function(z) {
+             zoo::na.approx(z, na.rm=FALSE)
+           },
+           min.length = 3L) {
+    x.na <- na.omit(x)
+    if (length(x.na) < min.length | length(x.na) == 0)
+      return(x)
+    if (is.na(x[1]))
+      x[1] <- x.na[1]
+    if (is.na(x[length(x)]))
+      x[length(x)] <- x.na[length(x.na)]
+    
+    do.call("fun", list(x))
+  }
+
+
+#' @rdname na_approx
+#' @export
+na_approx.data.frame <- function(x, ...) {
+as.data.frame(na_approx.matrix(x))
+ 
+}
+
+#' @rdname na_approx
+#' @export
+na_approx.matrix <- function(x, ...) {
+ rslt <-
+   t(apply(x, 1, na_approx.vector))
+ colnames(rslt) <-colnames(x)
+ row.names(rslt) <- row.names(x)
+ rslt
+}
+
