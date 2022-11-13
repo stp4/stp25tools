20e6c74e8f14ec767e90d1520e0ff1ef43fd5040
stp4 <w.peter@statistik-peter.at>
2022-11-13 09:22:26 +0100

    Fehler Kontrolle eingebaut.

--- R/separate_multiple_choice.R
+++ R/separate_multiple_choice.R
@@ -17,136 +17,149 @@
 #'
 #' @examples
 #'
-#'
-#'  lbl <- c (
-#' 'Heart Failure' = 1,
-#' 'Rhythm Abnormality' = 2,
-#' 'Valve Dysfunction' = 3,
-#' 'Bleeding with OAC' = 4,
-#' 'ACS' = 5,
-#' 'Neurological Event' = 6,
-#' 'Neoplastic Disease' = 7,
-#' 'Others' = 8,
-#' 'No Complications' = 0
+#'  #require(stp25tools)
+#' lbl <- c (
+#'   '1 Heart Failure' = 1,
+#'   '2 Rhythm Abnormality' = 2,
+#'   '3 Valve Dysfunction' = 3,
+#'   '4 Bleeding with OAC' = 4,
+#'   '5 ACS' = 5,
+#'   '6 Neurological Event' = 6,
+#'   '7 Neoplastic Disease' = 7,
+#'   '8 Others' = 8,
+#'   '0 No Complications' = 0
 #' )
-#'
+#' 
 #' x <- c(0,
-#'        "1,2,3,6,8",
+#'        "1,3,6,8,4",
 #'        2,
-#'        "1,2,8",
-#'        3.8,
-#'        "2,4,6,8")
-#'
-#' x <- gsub("\\.", ",", x)
-#'
-#' rslt <- separate_multiple_choice(x ,
-#'                                  sep = ",",
-#'                                  as_logical = TRUE,
-#'                                  label = lbl)
+#'        "",
+#'        "8,4",
+#'        #  3.8,100,
+#'        "4,6,8,3",
+#'        "2,3,4,5")
+#' 
+#' #x <- gsub("\\.", ",", x)
+#' rslt <-
+#'   separate_multiple_choice(x ,
+#'                            sep = ",",
+#'                            as_logical = TRUE,
+#'                            label = lbl)
 #' stp25stat2::Tbll_desc(rslt)
-#'
-#'
-#'
-#'
-#' dat <-  data.frame(Q1=c(134,NA,35,134,5,24),
+#' 
+#' 
+#' 
+#' dat <-  data.frame(Q1=c(134, NA, 35, 134, 5, 24),
 #'                    Q2=c(
-#'
 #'                      "Alm Dudler, Essig, Cola",
 #'                      NA,
 #'                      "Cola, Holer",
 #'                      "Alm Dudler, Cola, Essig",
 #'                      "Holer",
 #'                      "Bier, Essig"))
-#' #dat
-#'
-#'
-#' dat<- cbind(dat,separate_multiple_choice(dat$Q2))
-#'
-#' dat<- cbind(dat,separate_multiple_choice(dat$Q1,
-#'                                          label=c(
-#'                                            "Alm Dudler","Bier", "Cola", "Essig", "Holer") ))
-#' names(dat)<- GetLabelOrName(dat)
-#' dat
-#' x<-data.frame(A=c(15911,261011,3711,4811))
+#' 
+#' 
+#' 
+#' dat2 <- cbind(dat[2],
+#'               separate_multiple_choice(dat$Q2))
+#' names(dat2) <- get_label(dat2)
+#' dat2
+#' 
+#' 
+#' dat1 <- cbind(dat[1],
+#'               separate_multiple_choice(
+#'                 dat$Q1,
+#'                 label = 
+#'                   c("Alm Dudler", 
+#'                     "Bier", 
+#'                     "Cola", 
+#'                     "Essig", 
+#'                     "Holer")))
+#' names(dat1) <- get_label(dat1)
+#' dat1
+#' 
+#' 
+#' x<-data.frame(A=c(15911, 261011, 3711, 48111))
 #' separate_multiple_choice(x$A)
 separate_multiple_choice <- function(x,
-                                     sep = ", ",
-                                     #   levels,labels,
-                                     
-                                     out_levels = 1:0,
-                                     out_labels = c("ja", "nein"),
-                                     as_logical = FALSE,
-                                     prafix =  NULL,
-                                     into = NULL,
-                                     label = NULL,
-                                     na.strings = NULL) {
+                                      sep = ", ",
+                                      id = 1:length(x),
+                                      
+                                      out_levels = 1:0,
+                                      out_labels = NULL ,
+                                      #  c("ja", "nein"),
+                                      as_logical =  is.null(out_labels),
+                                      prafix =  NULL,
+                                      into = NULL,
+                                      label = NULL,
+                                      na.strings = NULL) {
   # callingFun = as.list(sys.call(-1))[[1]]
   # calledFun = as.list(sys.call())[[1]]
-  if (!is.null(na.strings))
-    x[which(x == na.strings)] <- NA
-  
-  if (is.null(prafix))
-    prafix <-
-      paste0(gsub("^.*\\$", "", deparse(substitute(x))), "_")
-  
-  
+  mssn <- "zz_9999"
   is_numeric_x <- FALSE
   
+  if (!is.null(na.strings))   x[which(x == na.strings)] <- NA
+  
+  if (is.null(prafix))  prafix <- paste0(gsub("^.*\\$", "", deparse(substitute(x))), "_")
+  prafix <- gsub("[^[:alnum:]_]", "", prafix)
+  
   if (is.numeric(x)) {
+    warning("Wenn als Codierung Zahlen verwendet werden geht das nur mit maximal 11 Attributen!")
     is_numeric_x <- TRUE
     x <- as.character(x)
     x <- gsub("10", "a", x)
     x <- gsub("11", "b", x)
-    
     x <- gsub("(.)\\B", "\\1,", x)
     sep <- ","
   }
+  else{
+    x <- as.character(x)
+  }
   
   
-  prafix <- gsub("[^[:alnum:]_]", "", prafix)
   #leere levels nicht  entfernen sondern behalten
-  x <- as.character(x)
-  x[is.na(x)] <- "XX_9999"
-  x[x==""] <- "XX_9999"
-  # if(missing(levels))
+  x[is.na(x)] <- mssn
+  x[x == ""] <- mssn
   x <- factor(x)
   
-  # else    x <- factor(x, levels)
+  
+  
   cat("\n----------------------------------------------------------------\n")
-  cat("Warnung: wenn komische Leerzeichen daher kommen gut aufpassen!\nDas was unten kommt wird aufgedröselt.\n")
-   print(levels(x))
-   cat("\n----------------------------------------------------------------\n")
+  cat("Warnung: wenn komische Leerzeichen daher kommen gut aufpassen!
+Das was unten kommt wird aufgedröselt.\n"
+  )
+  print(levels(x))
+  cat("\n----------------------------------------------------------------\n")
+  
+  
   # separate braucht die Anzahl an Levels
   unique_elements <-
     unique(unlist(stringr::str_split(x, sep)), use.names = FALSE)
   
-  #print(unique_elements)
   if (is.null(into))
-    into <- paste0("M", 1:length(unique_elements))
+    into <- paste("M", 1:length(unique_elements), sep = "")
   
   # id ist zur kontrolle und name xxxx damit es zu keinen konflikten kommt
-  res <-  data.frame(id = 1:length(x), xxxx = x)
-  
+  res <- data.frame(id = id, xxxx = x)
+  ## Aufsplitten
   res <-
     tidyr::separate(res,
                     "xxxx",
                     into = into,
                     sep = sep,
-                    remove = F)  ## Aufsplitten
+                    remove = FALSE)  
+  ## breit zu lang
   res <-
-    na.exclude(tidyr::gather(res, q2 , val, -id, -xxxx))   ## breit zu lang
-  
-  res <- tidyr::spread(dplyr::mutate(res, q2 = 1), val, q2)
-  
-  
+    na.exclude(tidyr::gather(res, q2 , val, -id, -xxxx))   
+  res <- 
+    tidyr::spread(dplyr::mutate(res, q2 = 1), val, q2)
   res[-1:-2][is.na(res[-1:-2])] <- 0
   
-  if (any(names(res) == "XX_9999")) {
-    res[which(res$XX_9999 == 1),-1] <- NA
-    res <- res[,-ncol(res)]
+  
+  if (any(names(res) == mssn)) {
+    res[which(res[[mssn]] == 1), -1] <- NA
+    res <- res[, -ncol(res)]
   }
-  
-  
   
   lbl <- names(res)[-1:-2]
   # Check if string contains ONLY NUMBERS
@@ -155,21 +168,24 @@ separate_multiple_choice <- function(x,
   else
     names(lbl) <- paste0(prafix, 1:length(lbl))
   
-  
   names(res)[-1:-2] <- names(lbl)
-  if (is.null(out_labels))
-    res <- res[-1:-2]
-  else
-    res <- dapply2(res[-1:-2],
-                   function(z) {
-                     if (as_logical)
-                       z == out_levels[1]
-                     else
-                       factor(z, out_levels, out_labels)
-                     
-                   })
   
+  # Return-Format
+  if (is.null(out_labels)) {
+    res <- res[-1:-2]
+    if (as_logical)
+      res <- dapply2(res, as.logical)
+  }
+  else{
+    res <-
+      dapply2(res[-1:-2], function(z)
+        factor(z, out_levels, out_labels))
+  }
+  
+  
+  # Ver-labeln
   if (!is.null(label) & is_numeric_x) {
+    
     lbl <- gsub("a", "10", lbl)
     lbl <- gsub("b", "11", lbl)
     
@@ -177,28 +193,51 @@ separate_multiple_choice <- function(x,
     y <- 1:length(label)
     used <- intersect(x, y)
     not_used <- setdiff(y, used)
-    
     names(res) <- paste0(prafix, used)
     for (i in   not_used) {
       res[paste0(prafix, i)] <- NA
     }
-    
-    
     names(label) <- paste0(prafix, 1:length(label))
-    
     cf <- names(res)
     res[order(nchar(cf), cf)]
-    
     res <-   set_label(res, label)
-    
-  } else{
+  }
+  else{
+    ## eigene labels
     if (is.null(label)) {
       res <-  set_label(res, lbl)
     }
     else if (!is.null(names(label))) {
-      lbl <- names(label)
-      names(lbl) <- paste0(prafix, as.vector(label))
-      res <-  set_label(res, lbl)
+      label  <-  factor(lbl, label, names(label))
+      lbl2 <- as.character(label)
+      names(lbl2) <- names(label)
+      
+      if (any(is.na(lbl2))) {
+        strange <- which(is.na(lbl2))
+        cat("\n----------------------------------------------------------------\n")
+        cat("Warnung: hier stimmt was nicht!\nNicht alle labels sind eindeutig!\n")
+        print( list(lbl=lbl,
+                    Label =lbl2,
+                    names = names(res),
+                    'Fehlend'= lbl[strange] ))
+        cat("\n----------------------------------------------------------------\n")
+        
+        stop(
+          "Achtung:\n  Nicht alle labels sind eindeutig!\n  Folgendes Label kommt nicht vor: ",
+          paste(lbl[strange] , collapse = ", ")
+        )
+        
+        lbl2[strange] <- lbl[strange]
+      }
+      
+      any_missing <- setdiff (names(lbl2) , names(res))
+      
+      if (length(any_missing) > 0) {
+        for (i in any_missing)
+          res[i] <- NA
+      }
+      
+      res <- set_label(res, lbl2)
     }
     else {
       nms < -names(lbl)
@@ -213,30 +252,192 @@ separate_multiple_choice <- function(x,
 }
 
 
+
 # require(stp25tools)
-#  lbl <- c (
-# 'Heart Failure' = 1,
-# 'Rhythm Abnormality' = 2,
-# 'Valve Dysfunction' = 3,
-# 'Bleeding with OAC' = 4,
-# 'ACS' = 5,
-# 'Neurological Event' = 6,
-# 'Neoplastic Disease' = 7,
-# 'Others' = 8,
-# 'No Complications' = 0
+# lbl <- c (
+#   '1 Heart Failure' = 1,
+#   '2 Rhythm Abnormality' = 2,
+#   '3 Valve Dysfunction' = 3,
+#   '4 Bleeding with OAC' = 4,
+#   '5 ACS' = 5,
+#   '6 Neurological Event' = 6,
+#   '7 Neoplastic Disease' = 7,
+#   '8 Others' = 8,
+#   '0 No Complications' = 0
 # )
 # 
 # x <- c(0,
-#        "1,2,3,6,8",
-#        2, "",
-#        "1,2,8",
-#        3.8,
-#        "2,4,6,8")
-# 
-# x <- gsub("\\.", ",", x)
+#        "1,3,6,8,4",
+#        2,
+#        "",
+#        "8,4",
+#        3.8,100,
+#        "4,6,8,3",
+#        "2,3,4,5")
+# length(x)
+# #x <- gsub("\\.", ",", x)
 # 
 # rslt <- separate_multiple_choice(x ,
-#                                  sep = ",",
-#                                  as_logical = TRUE,
-#                                  label = lbl)
+#                                   sep = ",",
+#                                   as_logical = TRUE,
+#                                   label = lbl)
 # stp25stat2::Tbll_desc(rslt)
+# 
+# 
+# 
+# separate_multiple_choice(c(1,2,3,4,10))
+
+
+
+
+
+# separate_multiple_choice_old <- function(x,
+#                                      sep = ", ",
+#                                      #   levels,labels,
+#                                      
+#                                      out_levels = 1:0,
+#                                      out_labels = c("ja", "nein"),
+#                                      as_logical = FALSE,
+#                                      prafix =  NULL,
+#                                      into = NULL,
+#                                      label = NULL,
+#                                      na.strings = NULL) {
+#   # callingFun = as.list(sys.call(-1))[[1]]
+#   # calledFun = as.list(sys.call())[[1]]
+#   if (!is.null(na.strings))
+#     x[which(x == na.strings)] <- NA
+#   
+#   if (is.null(prafix))
+#     prafix <-
+#       paste0(gsub("^.*\\$", "", deparse(substitute(x))), "_")
+#   
+#   
+#   is_numeric_x <- FALSE
+#   
+#   if (is.numeric(x)) {
+#     is_numeric_x <- TRUE
+#     x <- as.character(x)
+#     x <- gsub("10", "a", x)
+#     x <- gsub("11", "b", x)
+#     
+#     x <- gsub("(.)\\B", "\\1,", x)
+#     sep <- ","
+#   }
+#   
+#   
+#   prafix <- gsub("[^[:alnum:]_]", "", prafix)
+#   #leere levels nicht  entfernen sondern behalten
+#   x <- as.character(x)
+#   x[is.na(x)] <- "XX_9999"
+#   x[x==""] <- "XX_9999"
+#   # if(missing(levels))
+#   x <- factor(x)
+#   
+#   # else    x <- factor(x, levels)
+#   cat("\n----------------------------------------------------------------\n")
+#   cat("Warnung: wenn komische Leerzeichen daher kommen gut aufpassen!\nDas was unten kommt wird aufgedröselt.\n")
+#    print(levels(x))
+#    cat("\n----------------------------------------------------------------\n")
+#   # separate braucht die Anzahl an Levels
+#   unique_elements <-
+#     unique(unlist(stringr::str_split(x, sep)), use.names = FALSE)
+#   
+#   #print(unique_elements)
+#   if (is.null(into))
+#     into <- paste0("M", 1:length(unique_elements))
+#   
+#   # id ist zur kontrolle und name xxxx damit es zu keinen konflikten kommt
+#   res <-  data.frame(id = 1:length(x), xxxx = x)
+#   
+#   res <-
+#     tidyr::separate(res,
+#                     "xxxx",
+#                     into = into,
+#                     sep = sep,
+#                     remove = F)  ## Aufsplitten
+#   res <-
+#     na.exclude(tidyr::gather(res, q2 , val, -id, -xxxx))   ## breit zu lang
+#   
+#   res <- tidyr::spread(dplyr::mutate(res, q2 = 1), val, q2)
+#   
+#   
+#   res[-1:-2][is.na(res[-1:-2])] <- 0
+#   
+#   if (any(names(res) == "XX_9999")) {
+#     res[which(res$XX_9999 == 1),-1] <- NA
+#     res <- res[,-ncol(res)]
+#   }
+#   
+#   
+#   
+#   lbl <- names(res)[-1:-2]
+#   # Check if string contains ONLY NUMBERS
+#   if (!grepl("\\D", paste(lbl, collapse = "")))
+#     names(lbl) <- paste0(prafix, lbl)
+#   else
+#     names(lbl) <- paste0(prafix, 1:length(lbl))
+#   
+#   
+#   names(res)[-1:-2] <- names(lbl)
+#   if (is.null(out_labels))
+#     res <- res[-1:-2]
+#   else
+#     res <- dapply2(res[-1:-2],
+#                    function(z) {
+#                      if (as_logical)
+#                        z == out_levels[1]
+#                      else
+#                        factor(z, out_levels, out_labels)
+#                      
+#                    })
+#   
+#   if (!is.null(label) & is_numeric_x) {
+#     lbl <- gsub("a", "10", lbl)
+#     lbl <- gsub("b", "11", lbl)
+#     
+#     x <- as.numeric(lbl)
+#     y <- 1:length(label)
+#     used <- intersect(x, y)
+#     not_used <- setdiff(y, used)
+#     
+#     names(res) <- paste0(prafix, used)
+#     for (i in   not_used) {
+#       res[paste0(prafix, i)] <- NA
+#     }
+#     
+#     
+#     names(label) <- paste0(prafix, 1:length(label))
+#     
+#     cf <- names(res)
+#     res[order(nchar(cf), cf)]
+#     
+#     res <-   set_label(res, label)
+#     
+#   } else{
+#     if (is.null(label)) {
+#       res <-  set_label(res, lbl)
+#     }
+#     else if (!is.null(names(label))) {
+#       lbl <- names(label)
+#       names(lbl) <- paste0(prafix, as.vector(label))
+#       res <-  set_label(res, lbl)
+#     }
+#     else {
+#       nms < -names(lbl)
+#       lbl <- label
+#       names(lbl) <- nms
+#       res <-  set_label(res, lbl)
+#     }
+#     
+#   }
+#   
+#   res
+# }
+# 
+# 
+#  
+# 
+# 
+# 
+# 
+#  
