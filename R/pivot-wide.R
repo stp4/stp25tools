80df48d0da1babed7f7d9d356c2ff728bf62c92d
stp4 <w.peter@statistik-peter.at>
2023-10-29 10:28:41 +0100

    Neu geschrieben und jetzt intuitiver handbar aber die alten Files geht nicht mehr!

--- R/pivot-wide.R
+++ R/pivot-wide.R
@@ -1,77 +1,109 @@
-# Long
-
-
-
-
 #' @rdname Long
-#' @description  Wide  entspricht tidyr::spread()
 #'
-#' Quelle: https://community.rstudio.com/t/spread-with-multiple-value-columns/5378
+#' @param data dataframe
+#' @param key names or formula
+#' @param ...  values_fro or erster Teil der Formula
+#' @param values_fill,names_sep,names_vary an tidyr::pivot_wider
 #'
+#' @return tibble
 #' @export
 #'
 #' @examples
 #' 
-#' #  suppressPackageStartupMessages(library(tidyverse))
-#'
+#'   
 #' dat <- data.frame(
-#'   month = rep(1:3, 2),
-#'   student = factor(rep(c("Amy", "Bob"), each = 3)),
-#'   A = c(9, 7, 6, 8, 6, 9),
-#'   B = c(6, 7, 8, 5, 6, 7)
+#' month = rep(1:3, 2),
+#' student = factor(rep(c("Amy", "Bob"), each = 3)),
+#' A = c(19, 27, 16, 28, 10, 29),
+#' B = c(6, 7, 8, 5, 6, 7)
 #' )
-#'
-#' dat |> Wide(student,  c(A, B))
-#' dat |> Wide(student,  c("A", "B"))
-#' dat[-3] |> Wide(student,  B)
-#' dat  |> Wide(student ~ month)
-#'  #dat[-3] |> reshape2::dcast(month ~ student)
-#' dat  |> Wide(month ~ student, A)
-#' dat  |> Wide(student ~ month, A)
-#'
-Wide <- function(...) {
-  UseMethod("Wide")
-}
-
-#' @rdname Long
-#' @export
-Wide.formula <- function(x,
-                         data,
-                         value) {
-  valueq <- rlang::enquo(value)
-  value_names <-
-    paste(rlang::quo_get_expr(valueq))
+#' #dat %>% Wide(student,  A, B)
+#' # dat %>% Wide( month ~ student, value="A")
+#' dat %>% Wide(student)
+#' dat %>% Wide(student,  B)
+#' 
+#' dat %>% Wide(A ~ student)
+#' 
+#' dat %>% Wide(A + B ~ student)
+#' dat %>% Wide(A + B ~ student + month)
+#' 
+#' 
+#' dat %>% Wide(student,  A, B)
+#' 
+#' 
+#' dat %>% Wide(A ~ student)
+#' dat %>% Wide(A ~ student + month)
+#' 
+#' 
+#' dat %>% Wide(month ~ student, A)
+#' dat %>% Wide(month ~ student, A, B)
+Wide <- function(data,
+                  key,
+                  ...,
+                  values_fill = NULL,
+                  names_sep = "_",
+                  names_vary = "fastest") {
+  values_from <-
+    as.character(sapply(lazyeval::lazy_dots(...),
+                        function(x) {
+                          as.character(x[1])
+                        },
+                        simplify = TRUE))
   
-  if (value_names[1] == "")
-    value_names <- guess_value(data)
-  
-  x <-
-    clean_dots_formula(x, names_data = names(data))
-  
-  rhs <- all.vars(x[-2])
-  
-  if (length(value_names) == 1) {
-    data <- data[c(all.vars(x), value_names)]
-    data[value_names] <-  delet_label(data[value_names])
-    tidyr::pivot_wider(data,
-                       names_from = rhs,
-                       values_from = value_names)
-  } else{
-    stop("Das geht nicht! Alternative ist Wide(data, key,  c(A, B))")
+  # erlaubt verschiedene Schreibweisen
+  if (rlang::is_formula(data))  {
+    # my.formula <- data
+    names_from <- data
+    data <- key
+  }
+  else{
+    keyq <- rlang::enquo(key)
+    names_from <-  rlang::quo_get_expr(keyq)
+    if (rlang::is_formula(names_from)) {
+      if (length(values_from) > 0) {
+        if (length(values_from) == 1) {
+          return(wide_helper(data,!!key,!!values_from[1]))
+        }
+        else{
+          rslt <- wide_helper(data,!!key,!!values_from[1])
+          
+          for (i in seq_along(values_from[-1])+1) {
+            rslt <- dplyr::bind_rows(rslt,
+                                     wide_helper(data,!!key,!!values_from[i]),
+                                     .id = "which")
+          }
+          rslt$which <-
+            factor(rslt$which, seq_along(values_from), values_from)
+          return(rslt)
+        }
+      } else {
+        values_from <- all.vars(names_from[[2]])
+        names_from <- all.vars(names_from[[3]])
+      }
+    }
   }
   
+  if (length(values_from) == 0) {
+    values_from <- stp25tools:::guess_value(data)
+  }
+  # print(list(names_from = names_from,
+  #            values_from = values_from))
+  
+  tidyr::pivot_wider(
+    data,
+    names_from = !!names_from,
+    values_from = !!values_from,
+    values_fill = values_fill,
+    names_sep = names_sep,
+    names_vary = names_vary
+  )
   
 }
 
-
-#' @rdname Long
-#' @export
-Wide.data.frame <- function(data, key, value) {
-
-  keyq <- rlang::enquo(key)
-  # break value vector into quotes
-  valueq <- rlang::enquo(value)
+wide_helper <- function(data, key, value) {
   
+  keyq <- rlang::enquo(key)
+  valueq <- rlang::enquo(value)
   # Formula Interface -------------------------------------------------------
   value_names <- paste(rlang::quo_get_expr(valueq))
   key_names <-  rlang::quo_get_expr(keyq)
@@ -87,44 +119,114 @@ Wide.data.frame <- function(data, key, value) {
       data <- data[c(all.vars(key_names), value_names)]
       data[value_names] <-  delet_label(data[value_names])
       return(tidyr::pivot_wider(
-                    data,
-                    names_from = !!rhs,
-                    values_from = !!value_names))
+        data,
+        names_from = !!rhs,
+        values_from = !!value_names))
     } else{
       stop("Das geht nicht! Alternative ist Wide(data, key,  c(A, B))")
     }
   }
   
-  # spread ------------------------------------------------------------------
-  # test length value
-  if (length(rlang::quo_get_expr(valueq)) == 1) {
-    return(tidyr::pivot_wider(
-      data,
-      names_from = !!keyq,
-      values_from = !!valueq
-    ))
-  }
-  # multi-value -------------------------------------------------------------
-  
-  
-  
-  
-  ##  Ersätzen von tidyr::gather durch   pivot_longer
-  s <- rlang::quos(!!valueq)
-  dat_unite <-
-    tidyr::unite(tidyr::gather(data, variable, value,!!!s),
-                 temp,
-                 !!keyq,
-                 variable,
-                 sep = "_")
-  dat_unite$temp <- factor(dat_unite$temp,
-                           paste0(
-                             stringr::str_split(levels(factor(dat_unite$temp)), "_" , simplify = T)[, 1],
-                             "_",
-                             value_names[-1]
-                           ))
-  return(tidyr::pivot_wider(dat_unite, names_from = temp, values_from = value))
 }
+
+
+
+
+
+# Wide <- function(...) {
+#   UseMethod("Wide")
+# }
+# 
+# 
+# Wide.formula <- function(x,
+#                          data,
+#                          value) {
+#   valueq <- rlang::enquo(value)
+#   value_names <-
+#     paste(rlang::quo_get_expr(valueq))
+#   
+#   if (value_names[1] == "")
+#     value_names <- guess_value(data)
+#   
+#   x <-
+#     clean_dots_formula(x, names_data = names(data))
+#   
+#   rhs <- all.vars(x[-2])
+#   
+#   if (length(value_names) == 1) {
+#     data <- data[c(all.vars(x), value_names)]
+#     data[value_names] <-  delet_label(data[value_names])
+#     tidyr::pivot_wider(data,
+#                        names_from = rhs,
+#                        values_from = value_names)
+#   } else{
+#     stop("Das geht nicht! Alternative ist Wide(data, key,  c(A, B))")
+#   }
+#   
+#   
+# }
+
+
+
+# Wide.data.frame <- function(data, key, value) {
+# 
+#   keyq <- rlang::enquo(key)
+#   # break value vector into quotes
+#   valueq <- rlang::enquo(value)
+#   
+#   # Formula Interface -------------------------------------------------------
+#   value_names <- paste(rlang::quo_get_expr(valueq))
+#   key_names <-  rlang::quo_get_expr(keyq)
+#   
+#   if (rlang::is_formula(key_names)) {
+#     key_names <- clean_dots_formula(key_names, names_data = names(data))
+#     rhs <- all.vars(key_names[[3]])
+#     
+#     if (value_names[1] == "")
+#       value_names <- guess_value(data)
+#     
+#     if (length(value_names) == 1) {
+#       data <- data[c(all.vars(key_names), value_names)]
+#       data[value_names] <-  delet_label(data[value_names])
+#       return(tidyr::pivot_wider(
+#                     data,
+#                     names_from = !!rhs,
+#                     values_from = !!value_names))
+#     } else{
+#       stop("Das geht nicht! Alternative ist Wide(data, key,  c(A, B))")
+#     }
+#   }
+#   
+#   # spread ------------------------------------------------------------------
+#   # test length value
+#   if (length(rlang::quo_get_expr(valueq)) == 1) {
+#     return(tidyr::pivot_wider(
+#       data,
+#       names_from = !!keyq,
+#       values_from = !!valueq
+#     ))
+#   }
+#   # multi-value -------------------------------------------------------------
+#   
+#   
+#   
+#   
+#   ##  Ersätzen von tidyr::gather durch   pivot_longer
+#   s <- rlang::quos(!!valueq)
+#   dat_unite <-
+#     tidyr::unite(tidyr::gather(data, variable, value,!!!s),
+#                  temp,
+#                  !!keyq,
+#                  variable,
+#                  sep = "_")
+#   dat_unite$temp <- factor(dat_unite$temp,
+#                            paste0(
+#                              stringr::str_split(levels(factor(dat_unite$temp)), "_" , simplify = T)[, 1],
+#                              "_",
+#                              value_names[-1]
+#                            ))
+#   return(tidyr::pivot_wider(dat_unite, names_from = temp, values_from = value))
+# }
 
 
 
