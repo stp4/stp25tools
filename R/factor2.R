92e051bc748d2261eca5674ded7a9334cba40a02
stp4 <w.peter@statistik-peter.at>
2023-10-29 05:39:40 +0100

    neue Funktionalität

--- R/factor2.R
+++ R/factor2.R
@@ -26,16 +26,16 @@
 #'
 #'
 #' table(reorder2(x$sex, last = "other"))
-#' 
-#' 
+#'
+#'
 #' #' lattice::barchart(rev(reorder2(x$sex, last = "other")))
-#' 
-#' # dat<-as.data.frame(table(x$sex)) 
-#' # lattice::barchart( 
-#' #   reorder2(Var1, Freq, last="other") ~Freq, 
-#' #   dat, 
+#'
+#' # dat<-as.data.frame(table(x$sex))
+#' # lattice::barchart(
+#' #   reorder2(Var1, Freq, last="other") ~Freq,
+#' #   dat,
 #' #   origin =0)
-#' # 
+#' #
 #' # with(
 #' #   dat,
 #' #   reorder2(Var1, Freq, last="other"))
@@ -65,34 +65,76 @@ factor2 <- function(x,
 
 #' @rdname factor2
 #'
-#' @param X,... an reorder
+#' @param X,... an reorder  x => an atomic vector, usually a factor
+#' X	=> a vector of the same length as x, whose subset of values for each unique level of x determines the eventual order of that level.
 #' @param decreasing an ordere logical
 #' @param last character
+#' @param threshold Anzahl an minimalen Nennungen
 #'
 #' @export
+#'
+#' @examples
+#'
+#'  x <-
+#' c(
+#'   rep(1, 21),rep(2, 120),rep(3, 28),rep(4, 4),rep(5, 56),
+#'   rep(6, 2),rep(7, 92),rep(8, 42),rep(9, 74),rep(10, 20)
+#' )
+#'
+#' x <- factor(x, 1:10, letters[1:10])
+#' table(x)
+#' table(reorder2(x))
+#' table(reorder2(x, threshold = 30))
 #'
 reorder2 <- function(x,
                      X,
                      ...,
                      decreasing = TRUE,
-                     last = NULL) {
+                     last = NULL,
+                     threshold = NULL,
+                     threshold.na.strings = "Other") {
   if (missing(X)) {
-    x <-
-      factor(x, levels(x)[order (table(x), decreasing = decreasing)])
+    if (is.null(threshold)) {
+      x <-
+        factor(x, levels(x)[order(table(x), decreasing = decreasing)])
+    }
+    else {
+      xt <- table(x)
+      
+      if (is.na(threshold.na.strings)) {
+   
+        x <-
+          factor(x, names(sort(xt[xt > threshold], 
+                               decreasing = decreasing)))
+      }
+      else{
+        x.names <- names(sort(xt, decreasing = decreasing))
+        lvl <-
+          names(sort(xt[xt > threshold], decreasing = decreasing))
+        lvl <-
+          c(lvl , rep(threshold.na.strings, length(x.names) - length(lvl)))
+        
+        x <-
+          factor(x, x.names, lvl)
+        
+      }
+      
+    }
+    
     if (!is.null(last))
       for (ref in last)
         x <- relevel2(x, ref)
   }
   else{
     x <- reorder(x, X, ...)
-    if (!is.null(last)) 
+    if (!is.null(last))
       for (ref in last)
         x <- relevel(x, ref)
   }
   x
 }
 
-# helper 
+# helper
 # letzter level am ende
 # copie von relevel ohne fehlerpruefung
 relevel2 <- function(x,
@@ -103,3 +145,23 @@ relevel2 <- function(x,
   factor(x, levels = lev[c(seq_along(lev)[-ref], ref)], exclude = NULL)
 }
 
+# x <-
+#   c(
+#     rep(1, 21),
+#     rep(2, 120),
+#     rep(3, 28),
+#     rep(4, 4),
+#     rep(5, 56),
+#     rep(6, 2),
+#     rep(7, 92),
+#     rep(8, 42),
+#     rep(9, 74),
+#     rep(10, 20),
+#     NA
+#   )
+# 
+# x <- factor(x, 1:10, letters[1:10])
+# # table(addNA(x, ifany = FALSE))
+# # table(x)
+# # table(reorder2(x))
+# table(reorder2(x, threshold = 30))
