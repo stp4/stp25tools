732b9dd9a4535641e5029420337923e95b43e416
stp4 <w.peter@statistik-peter.at>
2024-01-07 07:24:08 +0100

    nix

--- R/pivot-dapply.R
+++ R/pivot-dapply.R
@@ -58,15 +58,8 @@ Dapply.data.frame <- function(x,
                               ...,
                               fun = function(y) as.numeric(y),
                               stringsAsFactors = FALSE) {
-  cat("\n in Dapply.data.frame\n")
- # print(head(x))
   X <- prepare_data2(x, ...)
-  cat("\n input: \n")
-  print(X$x[X$measure.vars])
-  
-  apply_data <- dapply2(X$x[X$measure.vars], fun)
-  cat("\n apply_data: \n")
-  print(head(  apply_data  ))
+  apply_data <- dapply2(X$data[X$measure.vars], fun)
   x[, X$measure.vars] <- apply_data
   x
 }
@@ -106,11 +99,6 @@ dapply1 <-
             fun = function(x) as.numeric(x),
             stringsAsFactors = FALSE,
             ...) {
-    
-    cat("\nin dapply1:\n")
-    cat( "\ndas habe ich bekommen...\n")
-    print(head(x))
-    cat("\n\n")
     if (tibble::is_tibble(x))
       tibble::as_tibble(
         plyr::llply(x, fun, ...))
